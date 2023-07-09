07358dd52effed8c62796ea03edbc00770d1096b
Finn Hartshorn <finnhartshorn@gmail.com>
2023-07-09 15:45:04 +1000

    Added ghidra function import script

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # Ghidra scripts
 
-A collection of scripts to aid analysis of Zoo Tycoon
+A collection of scripts to aid analysis of Zoo Tycoon. All Ghidra script require [Ghidrathon](https://github.com/mandiant/Ghidrathon)
 
 ## Scripts
 
 ### ida_export.idc
-This script exports demangled function names from IDA/IDA Pro for importing into Ghidra
+This script exports demangled function names from IDA/IDA Pro for importing into Ghidra. Based on [this script](https://gist.github.com/hax0kartik/e358ce447a4537bcef534aa8de84817c) which does not demangle before export.
+
+### ghidra_function_name_import.py
+Imports function names from a file containing two columns of function names and addresses. Function names have spaces and brackets and there contents removed. Addresses are in hex format. For very large files the script may need to be run multiple times.
---
+++ ghidra_function_name_import.py
@@ -0,0 +1,75 @@
+#TODO Imports function names from a file containing two columns of function names and addresses. Function names have spaces and brackets and there contents removed. Addresses are in hex format. For very large files the script may need to be run multiple times.
+#@author Finn Hartshorn
+#@category _NEW_
+#@keybinding 
+#@menupath 
+#@toolbar 
+
+
+import os.path
+
+from ghidra.program.model.symbol import SourceType #.SourceType import USER_DEFINED
+
+def remove_text_inside_brackets(text, brackets="()[]"):
+    count = [0] * (len(brackets) // 2) # count open/close brackets
+    saved_chars = []
+    for character in text:
+        for i, b in enumerate(brackets):
+            if character == b: # found bracket
+                kind, is_close = divmod(i, 2)
+                count[kind] += (-1)**is_close # `+1`: open, `-1`: close
+                if count[kind] < 0: # unbalanced bracket
+                    count[kind] = 0  # keep it
+                else:  # found bracket to remove
+                    break
+        else: # character is not a [balanced] bracket
+            if not any(count): # outside brackets
+                saved_chars.append(character)
+    return ''.join(saved_chars)
+
+def log_line(file_obj, line):
+    file_obj.writelines([line, "\n"])
+
+functionManager = currentProgram.getFunctionManager()
+
+f = askFile("Give me a file to open", "Go baby go!")
+
+file_name = f.getAbsolutePath()
+file_name_no_extension, extension = os.path.splitext(file_name)
+log_file_name = file_name_no_extension + ".log"
+
+with open(file_name, "r") as ida_export, open(log_file_name, "w") as ida_import_log:
+    for line in ida_export:
+        pieces = line.split(" 0x")
+        if len(pieces) != 2:
+            print("Error parsing symbol {} ({})".format(line, len(pieces)))
+            continue
+
+        name = remove_text_inside_brackets(pieces[0], brackets="()").replace(" ", "")
+        
+        address = toAddr("0x" + pieces[1]).add(int("0x10000000", 16))
+
+        func = functionManager.getFunctionAt(address)
+
+        if func is not None:
+            old_name = func.getName()
+            if old_name == name:
+                continue
+            try:
+                if name.startswith(".") and not old_name.startswith("FUN"):
+                    log_line(ida_import_log, "Not renaming {} to {}".format(old_name, name))
+                    continue
+                func.setName(name, SourceType.USER_DEFINED)
+                log_line(ida_import_log, "Renamed function {} to {} at address {}".format(old_name, name, address))
+            except Exception as e:
+                print("Failed to rename function from {} to {} because {}".format(old_name, name, e))
+                log_line(ida_import_log, "Failed to rename function from {} to {}".format(old_name, name))
+                continue
+        else:
+            try:
+                func = createFunction(address, name)
+                log_line(ida_import_log, "Created function {} at address {}".format(name, address))
+            except:
+                print("Failed to create function {}".format(name))
+                log_line(ida_import_log, "Failed to create function {}".format(name))
+                continue 
